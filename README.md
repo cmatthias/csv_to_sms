6bf91854a414c12c20f87e7bd3f5387ac6755182
Colin Matthias <cmatthias@everquote.com>
2018-10-31 15:03:25 -0400

    Add doc and skeleton code

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Ensure you have bundler installed
+1. `bundle install`
+1. set up ENV file (we'll call it `my.env`)
+1. `source my.env && bundle exec sms.rb`
--- sms.rb
+++ sms.rb
@@ -1,2 +1,21 @@
 require 'twilio-ruby'
+require 'csv'
 
+account_sid = ENV['ACCOUNT_SID']
+auth_token = ENV['AUTH_TOKEN']
+from_numbers = ENV['FROM_NUMBERS']
+client = Twilio::REST::Client.new(account_sid, auth_token)
+
+begin
+    froms = from_numbers.split(',')
+    raise ValueError if froms.empty?
+rescue
+    puts "ENV['FROM_NUMBERS'] must be a comma-delimited set of numbers to send from"
+    exit
+end
+
+client.messages.create(
+from: from,
+to: to,
+body: "Hey friend!"
+)
