07bf7c7d9795ff96937985a478493d6d5663eddf
Colin Matthias <cmatthias@everquote.com>
2018-10-31 16:38:57 -0400

    First tested version, works on my machine (tm)

--- sms.rb
+++ sms.rb
@@ -7,19 +7,38 @@ auth_token = ENV['AUTH_TOKEN']
 from_numbers = ENV['FROM_NUMBERS']
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
-#begin
+begin
     froms = from_numbers.split(',')
     raise ValueError if froms.empty?
-#rescue
-##    puts "ENV['FROM_NUMBERS'] must be a comma-delimited set of numbers to send from"
-#  exit
-#end
-puts "Froms: #{froms}"
+rescue
+    puts 'ENV[\'FROM_NUMBERS\'] must be a comma-delimited set of numbers to send from'
+    exit
+end
 
-# number,message
-# number,message
-#  etc...
 messages_to_send = CSV.read('sms.csv')
+
+CSV.open('sms_sent.csv', 'wb') do |out_csv|
+    puts ['from','to','body','response_code','errs'].map{|e|'"'+e+'"'}.join(',')
+    messages_to_send.each do |to, body|
+        from = froms.sample
+        #r = { :from => from, :to => to, :body => body }
+        #puts r.to_s
+        begin
+            error_code = ''
+            error_message = ''
+            client.messages.create(
+                from: from,
+                to: to,
+                body: body
+            )
+        rescue Twilio::REST::RequestError => e
+            error_code = e.code
+            error_message = e.message
+        ensure
+            puts [from, to, body, error_code, error_message].map{|e|'"'+e+'"'}.join(',')
+        end
+    end
+end
 
 #client.messages.create(
 #from: from,
