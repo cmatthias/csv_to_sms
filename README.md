fa048cc3d861fcab7537c220e44f3c4d4a7b37c9
Colin Matthias <cmatthias@everquote.com>
2018-10-31 15:25:25 -0400

    updates

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 1. Ensure you have bundler installed
 1. `bundle install`
-1. set up ENV file (we'll call it `my.env`). Required values:
+1. set up `.env` file. Required values:
     * `ACCOUNT_SID=ACxxxxxxxxxxxxxxxxxxxx`
     * `AUTH_TOKEN=xxxxxxxx`
     * `FROM_NUMBERS=1234567890,2345678901`
+       This last one should be a comma-delimited list of valid Twilio numbers to send from
 1. Put your messages in a CSV file called `sms.csv`. Each line should look like `to_number,message`, e.g.:
 ```
 "1234561111","Hello there"
 "1233759999","Hello to you as well"
 ```
-1. `source my.env && bundle exec sms.rb`
+1. `bundle exec sms.rb`
--- sms.rb
+++ sms.rb
@@ -1,21 +1,28 @@
 require 'twilio-ruby'
 require 'csv'
+require 'dotenv/load'
 
 account_sid = ENV['ACCOUNT_SID']
 auth_token = ENV['AUTH_TOKEN']
 from_numbers = ENV['FROM_NUMBERS']
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
-begin
+#begin
     froms = from_numbers.split(',')
     raise ValueError if froms.empty?
-rescue
-    puts "ENV['FROM_NUMBERS'] must be a comma-delimited set of numbers to send from"
-    exit
-end
+#rescue
+##    puts "ENV['FROM_NUMBERS'] must be a comma-delimited set of numbers to send from"
+#  exit
+#end
+puts "Froms: #{froms}"
 
-client.messages.create(
-from: from,
-to: to,
-body: "Hey friend!"
-)
+# number,message
+# number,message
+#  etc...
+messages_to_send = CSV.read('sms.csv')
+
+#client.messages.create(
+#from: from,
+#to: to,
+#body: "Hey friend!"
+#)
